faeb80552ce94b2f5621f3a7a31f984808b73718
Zachary Eddy <zeddy0226@gmail.com>
2016-12-05 21:59:07 -0800

    removed url option from configuration

--- configuration.py
+++ configuration.py
@@ -1,12 +1,11 @@
 # visit https://developer.github.com/v3/issues/#parameters to see what
 # 'tracked_keys' and 'state' get used for
 config = {
-    "url": "https://api.github.com/repos/rails/rails/issues",
     "repo": "rails",
 	"owner": "rails",
-	"access_token": "[insert your own]",
-	"max_requests": 2,
-	"per_page": 100,
+	"access_token": "[insert access token]",
+	"max_requests": 1,
+	"per_page": 20,
 	"tracked_keys": ["body", "title","number","labels","comments_url"],
     "ignored_labels": ["stale","attached PR","needs feedback","good first patch","needs backport","onhold","pinned","With reproduction steps"],
     "data_filename":"rails_data.json",
